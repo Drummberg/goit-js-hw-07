159c9298a43924c2b0ff137c02f2c6d13c977ced
Drummberg <drummberg@gmail.com>
2022-01-19 22:12:41 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryCards = createGalleryCards(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCards);
+
+function createGalleryCards(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="large-image.jpg"
+            alt="${description}"
+            />
+            </a>
+        </div>
+        `;
+        }).join('');
+       
+}
+
+
