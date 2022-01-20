377ec4bcaf2bdaf758fccaa32e1705032e21d6af
Drummberg <drummberg@gmail.com>
2022-01-20 23:10:38 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryCards = createGalleryCards(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryCards);
+
+function createGalleryCards(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" 
+        alt="${description}" />
+        </a>
+        `;
+        }).join('');
+       
+}
